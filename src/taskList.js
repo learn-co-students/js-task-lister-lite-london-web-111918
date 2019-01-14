bc9345817f3dc9aa6d5b0f0732525eb423812426
Andy <andypurbrick@gmail.com>
2019-01-14 17:07:03 +0000

    Done.

--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,21 @@
+
+// const clearAllEl = document.querySelector('#clear-all-tasks')
+
 class TaskList {
+  constructor() {
+    this.tasks = [];
+  }
+
+  createNewTask(description) {
+    const newTask = new Task(description);
+    this.tasks.push(newTask);
+  }
+
+  renderTasks() {
+    return this.tasks.map((task) => task.render()).join("");
+  }
+
+  deleteTask(description) {
+    this.tasks = this.tasks.filter((task) => task.description !== description);
+  }
 }
