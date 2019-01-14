7dfc642ecfe513c9eefe93331b456e699b800867
IMH51 <ianmartinharrison@outlook.com>
2019-01-14 15:57:35 +0000

    completed js task lister lite

--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,17 @@
 class TaskList {
+  constructor(){
+
+  }
+  renderSingleTask(newTask) {
+    const taskEl = document.createElement('li')
+    taskEl.className = 'card'
+    taskEl.innerText = newTask.text
+    taskListEl.appendChild(taskEl)
+  }
+
+  handleFormSubmit (event, obj) {
+    event.preventDefault()
+    let newTask = new Task(taskNameInputEl.value)
+    obj.renderSingleTask(newTask)
+  }
 }
